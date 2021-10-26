2653b52e60cbcd88265d7939243dfc3f2aa38f7b
csindex <jornelb@gmail.com>
2021-10-26 17:36:39 +0800

    Added a comment on 'main.dart' file.

--- theoria_riverpod_sample/lib/main.dart
+++ theoria_riverpod_sample/lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart' as p;
+// import 'package:provider/provider.dart' as p;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import './src/stateful/main-app.dart';
-import './src/stateful/features/pokemons/data/viewmodel/pokemon-list-viewmodel.dart';
-import './src/stateful/features/pokemons/ui/pokemon/pokemon-home-page.dart';
+// import './src/stateful/main-app.dart';
+// import './src/stateful/features/pokemons/data/viewmodel/pokemon-list-viewmodel.dart';
+// import './src/stateful/features/pokemons/ui/pokemon/pokemon-home-page.dart';
 import './src/riverpod/main-app.dart' as r;
 
 void main() {
   runApp(
     _providerScope,
+    // _multiProvider,
+    // comment '_providerScope' and uncomment '_multiProvider'
+    // to see my first solution where I use setState.
   );
 }
 
-p.MultiProvider get _multiProvider => p.MultiProvider(
+// Uncomment block to use _multiProvider.
+// Also uncomment necessary imports.
+/*p.MultiProvider get _multiProvider => p.MultiProvider(
   providers: [
     p.ChangeNotifierProvider(
       create: (context) => PokemonListViewModel(),
@@ -21,7 +26,7 @@ p.MultiProvider get _multiProvider => p.MultiProvider(
     ),
   ],
   child: PokemonMainApp(),
-);
+);*/
 
 ProviderScope get _providerScope => ProviderScope(
   child: r.PokemonMainApp(),
